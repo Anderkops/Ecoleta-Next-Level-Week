4c81ef799b7d0d93386a514b9245e7004ce743b7
Anderson <and_first@yahoo.com.br>
2020-06-23 00:13:34 -0300

    Criada rotas e SPA com Link entre a pagina de cadastro e a main page!

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <h1> Conteudo da aplicacao </h1>
-    </div>
+  return (   
+    <Routes />
   );
 }
-
 export default App;
